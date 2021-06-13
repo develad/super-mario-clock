ee3bc5eb5254d76a68f1d48f8170c1347070b333
develad <develadb@gmail.com>
2021-06-13 13:23:46 +0300

    :tada: adding moments js

--- app.js
+++ app.js
@@ -20,13 +20,16 @@ function jump(jumpItem) {
 
 function getTime() {
   const dateTime = new Date();
+  const currentTime = moment(dateTime).format('HH:mm:ss').split(':');
+  const [hours, minutes, seconds_str] = currentTime;
   // console.log(dateTime.toTimeString());
-  const timeString = dateTime.toLocaleTimeString();
-  let [hours, minutes, seconds_str] = timeString.split(':');
-  +hours < 10 ? (hours = `0${hours}`) : hours;
+  // const timeString = dateTime.toLocaleTimeString();
+  // let [hours, minutes, seconds_str] = timeString.split(':');
+  // +hours < 10 ? (hours = `0${hours}`) : hours;
   clock.innerHTML = `${hours} : ${minutes}`;
-  +seconds_str < 10 ? (seconds_str = `0${+seconds_str}`) : +seconds_str;
-  seconds.innerHTML = `${seconds_str.split(' ')[0]}`;
+  // +seconds_str < 10 ? (seconds_str = `0${+seconds_str}`) : +seconds_str;
+  // seconds.innerHTML = `${seconds_str.split(' ')[0]}`;
+  seconds.innerHTML = `${seconds_str}`;
   pageTitle.innerHTML = `${hours} : ${minutes}`;
 }
 
