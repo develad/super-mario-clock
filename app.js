101a3f1c79109c96483dd241adaa10a5dc765bd9
develad <develadb@gmail.com>
2021-12-18 15:46:39 +0200

    style change adding heart and score

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const clock = document.querySelector('#clock');
 const mlLeft = document.querySelector('.ml-left');
 const mlRight = document.querySelector('.ml-right');
 const seconds = document.querySelector('.seconds');
+const _score = document.querySelector('.score');
+const _heart = document.querySelector('#heart');
 const oneUp = new Audio('./smb_fireball.wav');
 
 mlLeft.addEventListener('click', () => jump(mlLeft));
 mlRight.addEventListener('click', () => jump(mlRight));
 
+let score = 0;
+
 function jump(jumpItem) {
+  _score.innerHTML = `score: ${++score}`;
+  if (score % 5 === 0) {
+    _heart.classList.add('show-heart');
+    setTimeout(() => {
+      _heart.classList.remove('show-heart');
+    }, 2000);
+  }
   jumpItem.classList.toggle('jump');
   seconds.classList.toggle('bang');
   oneUp.play();
